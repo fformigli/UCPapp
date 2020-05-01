dcf4c610c8b48b2b1f58c26f2b29ec4ce1bfa6c6
rminarro <herrrubenminarro@gmail.com>
2020-05-01 15:58:17 -0400

    Se agrego el modulo de financiero que reemplaza a Asistencias. Ademas de la base para la interfaz de Contacto.

---
+++ EssentialUIKit/Models/InscripcionResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialUIKit.Models
+{
+    public class MateriaInscripcionAlumno : Materia
+    {
+        public string CantidadDeAusencias { get; set; }
+        public string PorcentajeDeAsistencia { get; set; }
+        public string TopeDeAusencias { get; set; }
+
+    }
+
+    public class InscripcionResult
+    {
+        public List<MateriaInscripcionAlumno> MateriaInscripcionAlumno { get; set; }
+    }
+}
---
+++ EssentialUIKit/ViewModels/InscripcionPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms;
+using EssentialUIKit.Models;
+using EssentialUIKit.DataService;
+
+namespace EssentialUIKit.ViewModels
+{
+    [Preserve(AllMembers = true)]
+    class InscripcionPageViewModel : ContentPage
+    {
+        public List<MateriaAsistenciaAlumno> Materias { get; set; }
+
+        public InscripcionPageViewModel()
+        {
+            Materias = new List<MateriaAsistenciaAlumno>();
+
+            InitInscripciones();
+
+        }
+
+        private void InitInscripciones()
+        {
+            var api = new RestAPI();
+            api.GetAsistencia();
+
+            Materias.Clear();
+            Materias = RestAPI.AsistenciaResponse.MateriaAsistenciaAlumno;
+        }
+    }
+}
